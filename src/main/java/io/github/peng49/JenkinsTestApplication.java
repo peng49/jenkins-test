135a2063210f435c54cbc57e98a03ea1a38c4fba
peng49 <1219955253@qq.com>
2021-07-23 12:11:17 +0800

    init

---
+++ src/main/java/io/github/peng49/JenkinsTestApplication.java
@@ -0,0 +1,20 @@
+package io.github.peng49;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsTestApplication {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "hello jenkins";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+}
